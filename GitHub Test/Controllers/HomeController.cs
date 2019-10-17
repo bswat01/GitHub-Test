35810ce2622cdc79d5866cff2d4ce4f9385f69c8
Ben <beswat14@gmail.com>
2019-10-16 22:28:19 -0600

    Added Home Controller

---
+++ GitHub Test/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitHub_Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //Added Controller
+            string iDidIt = "Yes";
+            return View();
+        }
+    }
+}
